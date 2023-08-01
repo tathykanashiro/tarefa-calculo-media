1d4de6b5354cd261089d46f66557060b5e0a4ac2
Tathiany Cristina Kanashiro Marquesani <tathymarquesani@gmail.com>
2023-08-01 10:56:16 -0300

    Tarefa calculo media

---
+++ Tarefa.java
@@ -0,0 +1,17 @@
+/**
+ * @autor tathiany.marquesani
+ */
+public class Tarefa {
+    public static void main(String args[]){
+
+        double nota1 = 7.6;
+        double nota2 = 9;
+        double nota3 = 5.5;
+        double nota4 = 3;
+
+        double mediaNota = (nota1 + nota2 + nota3 + nota4)/4;
+        System.out.println("A media das notas é: " + mediaNota);
+
+    }
+
+}
